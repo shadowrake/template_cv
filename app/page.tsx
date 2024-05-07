fc2716ba1fbda2c4c20ca65325b5c99099cce0bd
Herman Kristiansen <gghh@live.no>
2024-05-07 11:16:07 +0200

    delete mobile btn for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -9,25 +9,12 @@ export default function Home() {
   return (
     <main>
       <Disclosure as="nav" className="bg-white shadow sticky top-0 z-40" >
-        {({ open }) => (
           <>
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8  ">
               <div className="flex h-16 justify-between">
                 <div className="flex">
-                  <div className="-ml-2 mr-2 flex items-center md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                      <span className="absolute -inset-0.5" />
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                      )}
-                    </Disclosure.Button>
-                  </div>
                   <div className="flex flex-shrink-0 items-center text-black text-lg font-bold ">
-                    {/* set logo link here */}
+                    {/* set logo img-link here */}
                     <img
                       className="h-8 w-auto"
                       src=""
@@ -42,7 +29,6 @@ export default function Home() {
               </div>
             </div>
           </>
-        )}
       </Disclosure>
       <Profile />
       <Experince />
